d71ca58870c6874f879fa2b8708a535c64cb44c5
Om Patel <opat597@aucklanduni.ac.nz>
2024-03-23 22:12:23 +1300

    Check if capacity input is number and if whole integer

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -28,12 +28,25 @@ public class VenueHireSystem {
       // Check if the venue code does not exist in array already
       for (Venue venue : venuesList) {
           if (venue.getVenueCode().equals(venueCode)) {
-              MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage();
+              MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, venue.getVenueName());
               return;
           }
       }
-      
 
+      // Check if the capacity is a valid number
+      try{
+        double capacityDoubled = Double.parseDouble(capacityInput);
+        int capacity;
+        if (capacityDoubled == (int)capacityDoubled){
+          capacity = (int) capacityDoubled;
+        } else {
+          MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", "whole number" );
+          return;
+        } 
+      } catch (NumberFormatException e) {
+        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", "number" );
+        return;
+      }
       
   }
 
