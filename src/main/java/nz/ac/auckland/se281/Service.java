04e53d5ff87bbcbde9527acf4247d923a4a63e05
Om Patel <opat597@aucklanduni.ac.nz>
2024-04-09 13:27:18 +1200

    create abstract class for services

---
+++ src/main/java/nz/ac/auckland/se281/Service.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.se281;
+
+public abstract class Service {
+  protected String bookingReference;
+
+  public Service(String bookingReference) {
+    this.bookingReference = bookingReference;
+  }
+
+  public abstract void addService();
+}
