108dd042d071b749e0e539993c58d0aac2bd2616
Om Patel <opat597@aucklanduni.ac.nz>
2024-04-09 14:24:00 +1200

    clean up service class to be more useful

--- src/main/java/nz/ac/auckland/se281/Service.java
+++ src/main/java/nz/ac/auckland/se281/Service.java
@@ -1,16 +1,15 @@
 package nz.ac.auckland.se281;
 
 public abstract class Service {
+  protected String serviceName;
   protected String bookingReference;
+  protected int serviceCost;
 
-  public Service(String bookingReference) {
+  public Service(String bookingReference, String serviceName, int serviceCost) {
     this.bookingReference = bookingReference;
+    this.serviceName = serviceName;
+    this.serviceCost = serviceCost;
   }
 
-  public abstract void addService();
-
-  @Override
-  public String toString() {
-    return "Service{" + "bookingReference='" + bookingReference + '\'' + '}';
-  }
+  public abstract int calculateCost();
 }
